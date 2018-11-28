e1f40035c7e5151a1e65bc9eed1f217d616fac57
pqo1993 <pqoalla@gmail.com>
2018-11-29 00:26:37 +0400

    Event Calculator

---
+++ Events Exem2/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events_Exem2
+{
+    class ClassCounter
+    {
+        public delegate void MethodContainer();
+        public event MethodContainer Method;
+
+       public void Counter()
+        {
+            
+            for (int i = 0; i < 100; i++)
+            {
+                if (i == 10)
+                    Method();
+
+            }
+        }
+    }
+    class Hendler_1
+    {
+        public void Message()
+        {
+            Console.WriteLine("Hurry Up");
+        }
+    }
+    class Hendler_2
+    {
+        public void Message()
+        {
+            Console.WriteLine("Hurry Up 71!");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClassCounter classCounter = new ClassCounter();
+            Hendler_1 hendler_1 = new Hendler_1();
+            Hendler_2 hendler_2 = new Hendler_2();
+            classCounter.Method += hendler_1.Message;
+            classCounter.Method += hendler_2.Message;
+            classCounter.Counter();
+        }
+    }
+}
---
+++ Events/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Events
+{
+    class Program
+    {
+        public delegate double ResultOfNumbers(double x, double y);
+
+        class Calculator
+        {
+            public event ResultOfNumbers NumbersOf;
+            public void EventInvoke()
+            {
+                NumbersOf(15, 20);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Calculator calculator = new Calculator();
+            calculator.NumbersOf += Add;
+            calculator.NumbersOf += Sum;
+            calculator.NumbersOf += Mult;
+            calculator.NumbersOf += Divis;
+            calculator.NumbersOf += SmallistOddNumber;
+            calculator.EventInvoke();
+
+
+        }
+        public static double Add(double x, double y)
+        {
+            double res = x + y;
+            Console.WriteLine(res);
+            return res;
+        }
+        public static double Sum(double x, double y)
+        {
+            double res = ++x - y--;
+            Console.WriteLine(res);
+            return res;
+
+        }
+        public static double Mult(double x, double y)
+        {
+            double res = x * y;
+            Console.WriteLine(res);
+            return res;
+
+        }
+        public static double Divis(double x, double y)
+        {
+            if (y != 0)
+            {
+                double res = x / y;
+                Console.WriteLine(res);
+                return res;
+            }
+            else
+                return 0;
+        }
+        public static double SmallistOddNumber (double x,double y)
+        {
+            int i;
+            for (i = 1; i < x; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    Console.WriteLine(i);
+                    return i;
+                    break;
+                }
+                
+                
+            }
+            return i;
+        }
+      
+
+    }
+}
