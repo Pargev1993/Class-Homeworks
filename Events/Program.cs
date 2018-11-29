288fd3548bad66ded49251f0d219d91f53d8cb9c
Pargev <pqoalla@gmail.com>
2018-11-29 20:11:49 +0400

    Homeworc calculator

--- Events/Program.cs
+++ Events/Program.cs
@@ -12,42 +12,93 @@ namespace Events
 
         class Calculator
         {
+            
+
             public event ResultOfNumbers NumbersOf;
-            public void EventInvoke()
+            public void EventInvoke(double x,double y)
+                {
+                NumbersOf.Invoke(x,y);
+}
+            public  double YNumber(double y)
+                {
+                try{
+                Console.WriteLine("Enter Y number");  
+                 y=double.Parse(Console.ReadLine());
+                    }
+                catch(FormatException )
+                {
+                    Console.WriteLine("Please Enter Number!");
+                    
+                }
+                return y;
+                   
+                }
+
+            public double XNumber(double x)
             {
-                NumbersOf(15, 20);
+                try{
+                Console.WriteLine("Enter X number");
+                x=double.Parse(Console.ReadLine());
+                 }
+                catch(FormatException )
+                {
+                    Console.WriteLine("Please Enter Number!");
+                    
+                }
+                return x;
             }
         }
 
         static void Main(string[] args)
         {
             Calculator calculator = new Calculator();
-            calculator.NumbersOf += Add;
-            calculator.NumbersOf += Sum;
-            calculator.NumbersOf += Mult;
-            calculator.NumbersOf += Divis;
-            calculator.NumbersOf += SmallistOddNumber;
-            calculator.EventInvoke();
-
-
+            double x=0.0;
+            double y=0.0;
+            x=calculator.XNumber(x);
+            y=calculator.YNumber(y);
+            Console.WriteLine("Please Chose An Operation wich one you wont to do: +,-,*,/ ");
+           string Oper=Console.ReadLine();
+            switch (Oper)
+              {
+            
+                   case "-":
+                  calculator.NumbersOf+=Sum;
+                    calculator.EventInvoke(x,y);
+                    break;
+		case "*":
+                    calculator.NumbersOf+=Mult;
+              calculator.EventInvoke(x,y);
+                    break;
+                    case "/":
+                    calculator.NumbersOf+=Divis;
+                    calculator.EventInvoke(x,y);
+                    break;
+                    case "+":
+                    calculator.NumbersOf+=Add;
+                    calculator.EventInvoke(x,y);
+                    break;
+                                    
+               }
+            Console.ReadKey();
+            
         }
         public static double Add(double x, double y)
         {
             double res = x + y;
-            Console.WriteLine(res);
+            Console.WriteLine("Result of Operation: "+res);
             return res;
         }
         public static double Sum(double x, double y)
         {
-            double res = ++x - y--;
-            Console.WriteLine(res);
+            double res =x - y;
+           Console.WriteLine("Result of Operation: "+res);
             return res;
 
         }
         public static double Mult(double x, double y)
         {
             double res = x * y;
-            Console.WriteLine(res);
+         Console.WriteLine("Result of Operation: "+res);
             return res;
 
         }
@@ -56,7 +107,7 @@ namespace Events
             if (y != 0)
             {
                 double res = x / y;
-                Console.WriteLine(res);
+                Console.WriteLine("Result of Operation: "+res);
                 return res;
             }
             else
