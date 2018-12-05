fbef93293d9094e91880e84c53b50b9089d3f904
pqo1993 <pqoalla@gmail.com>
2018-12-06 00:20:59 +0400

    converter

---
+++ ISTC.Secondcours.Generic/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.Secondcours.Generic
+{
+    class Program
+    {
+        public static T Converter<T>(string str)
+        {
+            T result = default(T);
+            result = (T)Convert.ChangeType(str, typeof(T));
+            Console.WriteLine(str);
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            string str = "123";
+            Converter<double>(str);
+        }
+    }
+}
