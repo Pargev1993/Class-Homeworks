903ae46e32c2b265b706a7ea73d926053803588e
pqo1993 <pqoalla@gmail.com>
2019-01-06 20:01:54 +0400

    Class Exemple

---
+++ ISTC.codingschool.OOP.Class/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.codingschool.OOP.Class
+{
+    class Persone
+    {
+        public int Age;
+        public string Name;
+        public Persone( int Age,string Name)
+        {
+            this.Age = Age;
+            this.Name = Name;
+        }
+        public Persone() { }
+        public void Display()
+        {
+            Console.WriteLine("Name {0} : Age {1} ",Name,Age);
+        }
+       
+    }
+    class Program
+    {
+         static void SendAPersonByValue(Persone P)
+        {
+            P.Age = 99;
+            P = new Persone(25, "Nikki");
+        }
+        static void Main(string[] args)
+        {
+            Persone persone = new Persone
+            {
+                Age = 25,
+                Name = "Bob"
+            };
+            persone.Display();
+            SendAPersonByValue(persone);
+            persone.Display();
+
+        }
+    }
+}
