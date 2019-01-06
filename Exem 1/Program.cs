d3e4bbf6ae9f6f1755d3876ec70838e39d05a941
pqo1993 <pqoalla@gmail.com>
2019-01-06 20:45:50 +0400

    Nullable exemples ?

---
+++ Exem 1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exem_1
+{
+    class DatabaseReader
+    {
+        public int? numericValue = null;
+        public bool? boolValue = true;
+        public int? GetIntFromDatabase()
+        {
+            return numericValue;
+        }
+        public bool? GetBoolFromDatabase()
+        {
+            return boolValue;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DatabaseReader dr = new DatabaseReader();
+            int myData = dr.GetIntFromDatabase() ?? 100;
+            Console.WriteLine("Value of myData: {0}", myData);
+            Console.ReadLine();
+        }
+    }
+}
+
