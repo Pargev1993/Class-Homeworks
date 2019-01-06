98a0b25f0ae2d8b7945b8dcf8c11023680154cf8
pqo1993 <pqoalla@gmail.com>
2019-01-06 13:10:26 +0400

    Enum Types

--- ISTC.codingschool.class.Enum/Program.cs
+++ ISTC.codingschool.class.Enum/Program.cs
@@ -6,34 +6,43 @@ using System.Threading.Tasks;
 
 namespace Enumer
 {
-    enum Enumtype:byte
+    enum EmpType : byte
     {
-        manager,
-        worker,
-        director,
+        Manager = 10,
+        Grunt = 1,
+        Contractor = 100,
+        VicePresident = 9
     }
 
     class Program
     {
         static void Main(string[] args)
         {
-            Enumtype @enum = Enumtype.manager;
-            AskforBonus(@enum);
-            Console.WriteLine(Enum.GetUnderlyingType(@enum.GetType()));
+            Console.WriteLine("**** Fun with Enums *****");
+            EmpType e2 = EmpType.Contractor;
+            DayOfWeek day = DayOfWeek.Monday;
+            ConsoleColor cc = ConsoleColor.Gray;
+            EvaluateEnum(e2);
+            EvaluateEnum(day);
+            EvaluateEnum(cc);
+            Console.ReadLine();
         }
-        static void AskforBonus(Enumtype e)
-        {
-            switch (e)
-            {
-                case Enumtype.director:
-                    Console.WriteLine("Director");
-                    break;
-                case Enumtype.manager:
-                    Console.WriteLine("Manager");
-                    break;
 
+        static void EvaluateEnum(System.Enum e)
+        {
+            Console.WriteLine("=> Information about {0}", e.GetType().Name);
+            Console.WriteLine("Underlying storage type: {0}",
+            Enum.GetUnderlyingType(e.GetType()));
+            Array enumData = Enum.GetValues(e.GetType());
+            Console.WriteLine("This enum has {0} members.", enumData.Length);
+           
+            for (int i = 0; i < enumData.Length; i++)
+            {
+                Console.WriteLine("Name: {0}, Value: {0:D}", enumData.GetValue(i));
             }
+            Console.WriteLine();
         }
+
     }
 }
 
