f40235eff11a9627797e77c1b1d19e27643a6f95
pqo1993 <pqoalla@gmail.com>
2019-01-06 13:34:01 +0400

    Struct exemples

---
+++ ISTC.codingschool.OOP.Struct/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.codingschool.OOP.Struct
+{
+    struct Point
+    {
+        
+        public int X;
+        public int Y;
+        public void Increment()
+        {
+            X++;
+            Y++;
+        }
+        public  void Decrement()
+        {
+            X--;
+            Y--;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Result X= {0} , Y= {1}",X,Y);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point MyPoint;
+            MyPoint.X = 125;
+            MyPoint.Y = 123;
+            MyPoint.Display();
+            MyPoint.Increment();
+            MyPoint.Display();
+
+
+        }
+    }
+}
