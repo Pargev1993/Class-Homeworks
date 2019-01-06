34410bff007392819af158dadf5b488e01df8cb8
pqo1993 <pqoalla@gmail.com>
2019-01-06 12:34:24 +0400

    Enum

---
+++ ISTC.codingschool.class.Enum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enum
+{
+    enum Enum
+    {
+        manager,
+        worker,
+        director,
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Enum @enum = Enum.manager;
+            AskforBonus(@enum);
+        }
+        static void AskforBonus(Enum e)
+        {
+            switch (e)
+            {
+                case Enum.director:
+                    Console.WriteLine("Director");
+                    break;
+                case Enum.manager:
+                    Console.WriteLine("Manager");
+                    break;
+
+            }
+        }
+    }
+}
+
