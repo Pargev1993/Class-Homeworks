3d889cce1d280c965949de8e6edcb074a408f8c6
pqo1993 <pqoalla@gmail.com>
2019-01-07 22:21:00 +0400

    exemple

---
+++ Exem 3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exem_3
+{
+    class User
+    {
+        public Director director { get;set; }
+    }
+    class Director
+    {
+        public Human human { get; set; }
+    }
+    class Human
+    {
+        public string Name { get; set; } = "Smit";
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            string a = user?.director?.human?.Name?? "jon";
+            Console.WriteLine(a);
+            
+        }
+    }
+}
