10197704eff8ca1501a30ca5c695884ca2cc62be
pqo1993 <pqoalla@gmail.com>
2019-01-07 19:33:17 +0400

    Valyu Vs Ref

---
+++ AcssesMod/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValyuRef
+{
+    class Person
+    {
+        public string Name;
+        public int Age;
+    }
+    class Program
+    {
+        static void Change( ref Person person)
+        {
+
+            person.Name = "Jon";
+            person = new Person();
+            person.Name = "Jemy";
+            person.Age = 36;            
+        }
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Age = 25;
+            person.Name = "Bill";
+            Change(ref person);
+            Console.WriteLine(person.Age);
+            Console.WriteLine(person.Name);
+        }
+    }
+}
---
+++ Exem 4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exem_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
