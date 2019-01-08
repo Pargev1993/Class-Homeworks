0dbd1ede302dc22bcad2a72da4f7e1518ee9aa59
pqo1993 <pqoalla@gmail.com>
2019-01-08 14:31:26 +0400

    Info Aboute Drivers

---
+++ ISTC.codingschool.OOP.WorkWithFiles/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ISTC.codingschool.OOP.WorkWithFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DriveInfo[] driveInfos = DriveInfo.GetDrives();
+            foreach (var item in driveInfos)
+            {
+                Console.WriteLine(item.Name);
+                Console.WriteLine(item.DriveType);
+                if (item.IsReady)
+                {
+                    Console.WriteLine(item.TotalSize);
+                    Console.WriteLine(item.TotalFreeSpace);
+                    Console.WriteLine(item.VolumeLabel);
+                }
+            }
+
+        }
+    }
+}
