1705d4ed479b41428d1ff9cbaf8897bd81948ae3
pqo1993 <pqoalla@gmail.com>
2019-01-08 14:20:12 +0400

    Inhertianse

---
+++ ISTC.codingschool.OOP.inheritans/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.codingschool.OOP.inheritans
+{
+    class Person
+    {
+        public string Name { get; set; }
+        private int Age;
+        public Person(int age,string name)
+        {
+            Name = name;
+            Age = age;
+        }
+    }
+    class Director:Person
+    {
+        
+        public int Price { get; set; }
+        private double Hige;
+        public Director(double hige,string name,int age,int price):base(age,name)
+        {
+            Price = price;
+        }
+
+    }
+    class worker:Person
+    {
+        public string Vakanci { get; set; }
+        public worker(string name,int age,string vakanci):base(age,name)
+        {
+            Vakanci = vakanci;
+        }
+    }
+   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Director director = new Director(25, "Armen", 26, 100000);
+            Console.WriteLine(director);
+        }
+    }
+}
