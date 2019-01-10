99da916dee9dcfa643afd8127a0f33d3c66aae9c
pqo1993 <pqoalla@gmail.com>
2019-01-10 18:51:47 +0400

    Work with files

--- ISTC.Codingschool.OOP.WorkWithFiles2/Program.cs
+++ ISTC.Codingschool.OOP.WorkWithFiles2/Program.cs
@@ -7,23 +7,18 @@ using System.IO;
 
 namespace ISTC.Codingschool.OOP.WorkWithFiles2
 {
+   
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Plrase write a text");
-            string text = Console.ReadLine();
-
-            using (FileStream files = new FileStream(@"C:\Users\pqoal\Documents\txt.txt", FileMode.OpenOrCreate))
-            {
-                byte[] Array = System.Text.Encoding.Default.GetBytes(text);
-                files.Write(Array, 0, Array.Length);
-               var s= files.Position;
-                files.Close();
-            
-            }
-           
-          
+            int i = 1;
+            object obj = i;
+            ++i;
+            Console.WriteLine(i);
+            Console.WriteLine(obj);
+            Console.WriteLine((short)obj);
         }
     }
+
 }
