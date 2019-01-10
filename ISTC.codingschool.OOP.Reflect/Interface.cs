1c533af7b15d8c03b9227169c7dc6b4900d088f7
pqo1993 <pqoalla@gmail.com>
2019-01-10 23:39:14 +0400

    Reflection

---
+++ ISTC.codingschool.OOP.Reflect/Interface.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.codingschool.OOP.Reflect
+{public interface Intercase1
+    {
+        void MethodeA();
+    }
+}
---
+++ ISTC.codingschool.OOP.Reflect/Persone.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ISTC.codingschool.OOP.Reflect
+{
+     class Persone:Intercase1,Interface2
+    {
+        public string Name { get;  } = "Albert";
+        public int Age { get; private set; } = 25;
+        public Double Highe { get; private set; } = 169.25;
+        public void GetAge()
+        {
+            Console.WriteLine(Age);
+        }
+        public void GetName ()
+        {
+            Console.WriteLine(Name);
+        }
+        void Intercase1.MethodeA() { }
+        void Interface2.MethodB() { }
+        public Persone()
+        {
+
+        }
+        public Persone(string name,int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        private int MethodC(int age,int highe)
+        {
+            return age + (int)highe;
+        }
+
+    }
+}
