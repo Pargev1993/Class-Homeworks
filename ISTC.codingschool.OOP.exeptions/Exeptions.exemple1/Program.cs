f947701f96adaa650fc4ac73e7423445d1adbfe7
pqoalla <pqoalla@gmail.com>
2019-01-16 17:24:50 +0400

    Catch exeption

---
+++ ISTC.codingschool.OOP.exeptions/Exeptions.exemple1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Del(int x, int y)
+        {
+            try
+            {
+                int result = x / y;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Деление на ноль!");
+                throw;
+            }
+        }
+
+        static void Main()
+        {
+            try
+            {
+                Del(5, 0);
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Программная ошибка!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
