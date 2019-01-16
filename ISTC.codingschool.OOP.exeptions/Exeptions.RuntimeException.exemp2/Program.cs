5fc98bb39343b0ebfa52ee07961db702267d37ef
pqoalla <pqoalla@gmail.com>
2019-01-16 17:37:38 +0400

    Exeption

---
+++ ISTC.codingschool.OOP.exeptions/Exeptions.RuntimeException.exemp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ExceptionServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exeptions.RuntimeException.exemp2
+{
+
+    class Program : System.Attribute
+    {
+        static void RunMyApplication()
+        {
+            int s = 0;
+            int d = 20;
+            int z = s / d;
+        }
+        [HandleProcessCorruptedStateExceptions]
+        static int Main(string[] args)
+        {
+            try
+            {
+               
+                RunMyApplication();
+                throw new Exception();
+            }
+            catch (Exception ex)
+            {
+                
+                Console.WriteLine("error! ", ex.Message);
+                return -1;
+            }
+            return 0;
+        }
+    }
+}
